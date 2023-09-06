2d657387b894abec7da5c2e1ea32d9b486bc61f9
NoOneLevi <zxvitor1@hotmail.com>
2023-09-05 23:56:20 -0300

    first commit

---
+++ 4 - Aprendizagem Bayesiana/robot.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
+import pickle
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+base_credit = pd.read_csv("./credit_data.csv")
+
+base_credit["age"][base_credit["age"] > 0].mean()
+base_credit["age"].fillna(base_credit["age"].mean(), inplace=True)
+
+print(base_credit)
+
+x_credit = base_credit.iloc[:, 1:4].values
+y_credit = base_credit.iloc[:, 4]
+(
+    x_credit_treinamento,
+    x_credit_test,
+    y_credit_treinamento,
+    y_credit_test,
+) = train_test_split(x_credit, y_credit, test_size=0.25, random_state=0)
+
+with open("./credit.pkl", mode="wb") as f:
+    pickle.dump(
+        [x_credit_treinamento, y_credit_treinamento, x_credit_test, y_credit_test], f
+    )
+
+naive_credit_data = GaussianNB()
+naive_credit_data.fit(x_credit_treinamento, y_credit_treinamento)
+
+previsoes = naive_credit_data.predict(x_credit_test)
+
+print(previsoes)
